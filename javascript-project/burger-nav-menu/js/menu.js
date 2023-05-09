3470aa53424fa0561d49573debeefdd68c51588e
andrea <ducation.life@gmaile.com>
2023-05-09 22:49:37 +0200

    update js projects

--- javascript-project/burger-nav-menu/js/menu.js
+++
@@ -1,9 +0,0 @@
-
-let navMenu = document.querySelector('.nav');
-
-let toggle = document.querySelector('.nav-toggle');
-
-toggle.addEventListener('click',()=>{
-    console.log('yes');
-    navMenu.classList.toggle('show-nav');
-})
