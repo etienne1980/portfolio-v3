af81f701bda43076e7eba7eab7c49007fcf3aab8
andrea <ducation.life@gmail.com>
2023-07-27 11:02:21 +0200

    added card project placeholder react

--- js/myApp.js
+++ js/myApp.js
@@ -2,6 +2,8 @@
 
 let cardButtonInactive = document.querySelectorAll('.no-active');
 
+console.log(cardButtonInactive);
+
 cardButtonInactive.forEach((item)=>{
     item.addEventListener('click',(e)=>{
         e.preventDefault();
